3eb61889fa23ba675374b78bbeebfee2855a89ef
Alan Mejia <mejiamartinezalan@gmail.com>
2022-01-17 02:35:35 -0600

    Server configuration

---
+++ config-user-server/src/test/java/com/userserver/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userserver.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
